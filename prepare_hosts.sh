803bdd6f348e91383f2d9093976eb4d981b359f7
draks <barbosm@users.noreply.github.com>
2018-03-23 14:12:30 -0700

    update

--- prepare_hosts.sh
+++ prepare_hosts.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 devices=( "inet.fam"
-	  "t1_acct.fam" 
-	  "t1_bo01.fam" 
-	  "t1_mkt.fam")
+	  "t1_dc01.sdw" 
+	  "t1_dc02.sdw" 
+	  "t1_s01.sdw")
 
 for device in "${devices[@]}"
 do
