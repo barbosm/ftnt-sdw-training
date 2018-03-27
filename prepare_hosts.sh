91b26f442bcdd82a370de6771fabf4a135d1fe3f
draks <barbosm@users.noreply.github.com>
2018-03-27 09:31:21 -0700

    fixed prepare hosts

--- prepare_hosts.sh
+++ prepare_hosts.sh
@@ -10,11 +10,23 @@ devices=( "t1_dc01.sdw"
 	  "web1.sdw"
 	  )
 
+password="fortinet"
+
 for device in "${devices[@]}"
 do
+        
 	echo $device
-	ssh-copy-id fortinet@$device 
+	ssh-copy-id fortinet@$device
+	scp /root/ftnt-sdw-training/bastion_files/id_rsa.pub_bastion fortinet@$device:/home/fortinet/
+	scp /root/ftnt-sdw-training/bastion_files/id_rsa.key_bastion fortinet@$device:/home/fortinet/       
+	echo "$password" | ssh -t fortinet@$device "sudo -S mkdir /root/.ssh"
+	echo "$password" | ssh -t fortinet@$device "sudo -S cp id_rsa.pub_bastion /root/.ssh/authorized_keys"
+	echo "$password" | ssh -t fortinet@$device "sudo -S mv id_rsa.pub_bastion /root/.ssh/id_rsa.pub"
+	echo "$password" | ssh -t fortinet@$device "sudo -S mv id_rsa.key_bastion /root/.ssh/id_rsa"
+	echo "$password" | ssh -t fortinet@$device "sudo -S chown -R root:root /root/.ssh/"
 
 done
 
+
 # fix hosts route table
+
